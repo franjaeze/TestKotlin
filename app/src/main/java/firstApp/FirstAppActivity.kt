428cbcb970c64f25a8d70a245d24f10c1c4ebc8f
francisco Verón <fvwear@hotmail.com>
2023-08-25 20:25:13 -0300

    first challenge done!

--- app/src/main/java/firstApp/FirstAppActivity.kt
+++ app/src/main/java/firstApp/FirstAppActivity.kt
@@ -2,13 +2,26 @@ package firstApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.example.helloandroidstudio.R
 
 class FirstAppActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first_app2) // une el diseño con la logica
+        setContentView(R.layout.activity_first_app) // une el diseño con la logica
+
+        val btnStart=findViewById<AppCompatButton>(R.id.btnStart)
+        val editText=findViewById<AppCompatEditText>(R.id.editor)
+
+        val name = editText.text.toString()
+
+        btnStart.setOnClickListener{
+            if(name.isNotEmpty()){
+            Log.i("FranDev", "Button clicked ${name}")
+        }
+
     }
+}
 }
