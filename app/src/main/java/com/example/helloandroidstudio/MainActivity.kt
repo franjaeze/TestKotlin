a588f64c7c03b350b906951241057994b9c50425
francisco Verón <fvwear@hotmail.com>
2023-09-04 23:39:45 -0300

    second challange done!

--- app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/helloandroidstudio/MainActivity.kt
@@ -9,7 +9,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var navigation: BottomNavigationView;
-    private val navMenu = BottomNavigationView.OnNavigationItemSelectedListener { item->
+    private val navMenu = BottomNavigationView.OnNavigationItemSelectedListener { item ->
 
         when (item.itemId){
             R.id.itemFragment1 -> {
@@ -19,7 +19,25 @@ class MainActivity : AppCompatActivity() {
                     addToBackStack("replacemnet");
 
                 }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.itemFragment2 -> {
+                supportFragmentManager.commit {
+                    replace<SecondFragment>(R.id.frameContainer)
+                    setReorderingAllowed(true);
+                    addToBackStack("replacemnet");
 
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.itemFragment3 -> {
+                supportFragmentManager.commit {
+                    replace<ThirdFragment>(R.id.frameContainer)
+                    setReorderingAllowed(true);
+                    addToBackStack("replacemnet");
+
+                }
+                return@OnNavigationItemSelectedListener true
             }
         }
         false
@@ -27,6 +45,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigation = findViewById(R.id.nav_menu)
+        navigation.setOnNavigationItemSelectedListener (navMenu)
 
         supportFragmentManager.commit {
             replace<FirstFragment>(R.id.frameContainer)
